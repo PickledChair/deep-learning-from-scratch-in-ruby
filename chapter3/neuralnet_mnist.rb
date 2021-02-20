82c48fb12202ce677b27027fb882c08ee75cc456
PickledChair <ubatamamoon@gmail.com>
2021-02-20 12:53:01 +0900

    add neuralnet_mnist.rb

---
+++ chapter3/neuralnet_mnist.rb
@@ -0,0 +1,47 @@
+require '../dataset/mnist'
+require '../common/functions'
+
+def get_data
+  *, (x_test, t_test) = MNIST.load_mnist(normalize: true,
+                                         flatten: true,
+                                         one_hot_label: false)
+  return x_test, t_test
+end
+
+def init_network
+  data = File.binread("sample_weight.dump")
+  network = Marshal.load(data)
+  return network
+end
+
+def predict(network, x)
+  w1, w2, w3 = network['W1'], network['W2'], network['W3']
+  b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+  a1 = x.dot(w1) + b1
+  z1 = sigmoid(a1)
+
+  a2 = z1.dot(w2) + b2
+  z2 = sigmoid(a2)
+
+  a3 = z2.dot(w3) + b3
+  y = softmax(a3)
+
+  return y
+end
+
+if $0 == __FILE__
+  x, t = get_data
+  network = init_network
+  accuracy_cnt = 0
+
+  x.shape[0].times { |i|
+    y = predict(network, x[i, 0...784])
+    p = y.argmax
+    if p == t[i] then
+      accuracy_cnt += 1
+    end
+  }
+
+  puts "Accuracy: " + (accuracy_cnt.to_f / x.shape[0].to_f).to_s
+end
