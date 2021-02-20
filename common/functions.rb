3204e2924cb235210235aec5a90edc34898a8e70
PickledChair <ubatamamoon@gmail.com>
2021-02-20 10:36:32 +0900

    add functions (identity, step, sigmoid and relu) to common/functions.rb

--- common/functions.rb
+++ common/functions.rb
@@ -1,6 +1,22 @@
 require "numo/narray"
 NM = Numo::NMath
 
+def identity_function(x)
+  return x
+end
+
+def step_function(x)
+  return Numo::Int32.cast(x > 0)
+end
+
+def sigmoid_function(x)
+  return 1 / (1 + NM.exp(-x))
+end
+
+def relu(x)
+  return x.clip(0, nil)
+end
+
 def softmax(x)
   x = x - x.max
   return NM.exp(x) / NM.exp(x).sum
